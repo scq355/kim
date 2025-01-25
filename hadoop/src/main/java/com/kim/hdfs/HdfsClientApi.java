5f9b0a56a3a2e08a71fe3f68588458876cc1cadd
jimmy <nwpuscq355@163.com>
2025-01-25 20:24:21 +0800

    文件

---
+++ hadoop/src/main/java/com/kim/hdfs/HdfsClientApi.java
@@ -0,0 +1,10 @@
+package com.kim.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+
+public class HdfsClientApi {
+
+    public void testMkDirs() {
+        Configuration configuration = new Configuration();
+    }
+}
